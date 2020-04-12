8f09cdab753d5f265edc73c45ad532df12460a9e
mikeku <mikekuforgit@gmail.com>
2020-04-12 12:46:33 +0800

    將資料模型加入至Administration

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'tel')  # 顯示欄位
+
+
+admin.site.register(Customer, CustomerAdmin)  # 加入至Administration(管理員後台)
