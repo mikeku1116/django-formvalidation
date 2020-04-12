ccd3bac89734a0115db41385fa6a919418292c56
mikeku <mikekuforgit@gmail.com>
2020-04-12 14:03:27 +0800

    建立共用樣板及美化資料模型表單

--- customers/forms.py
+++ customers/forms.py
@@ -7,3 +7,13 @@ class CustomerModelForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'style': 'width:50%;'}),
+            'email': forms.TextInput(attrs={'class': 'form-control', 'style': 'width:50%;'}),
+            'tel': forms.TextInput(attrs={'class': 'form-control', 'style': 'width:50%;'})
+        }
+        labels = {
+            'name': '姓名',
+            'email': '電子郵件',
+            'tel': '聯絡電話'
+        }
