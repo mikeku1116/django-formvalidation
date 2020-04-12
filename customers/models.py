ed1e2592bd28e1717b00bae021457bd09f9fe123
mikeku <mikekuforgit@gmail.com>
2020-04-12 12:38:53 +0800

    建立資料模型及執行migration

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=30, blank=False, null=False)
+    email = models.EmailField(blank=False, null=False)
+    tel = models.IntegerField()
